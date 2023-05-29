ce5eb0aecf15c73605d8431f367a9af5866291f0
Andrewkizito <Andrewkizito54@gmail.com>
2023-05-29 11:07:55 +0300

    Initial Setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#3d9e4d',
+        solid: '#001d4c',
+      },
+    },
   },
   plugins: [],
 }
-
